7714eefdc4ca53ea92f4a16869d09429596661d7
nakyoutae <qppk@naver.com>
2020-03-19 08:59:19 +0900

    1

--- src/test/java/com/competition/three/ThreeTest.java
+++ src/test/java/com/competition/three/ThreeTest.java
@@ -1,23 +1,36 @@
 package com.competition.three;
 
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 public class ThreeTest {
 
 	@Test
-	public void test() {
+	public void test() throws URISyntaxException {
+		
+		URI uri = new URI("http://localhost:8080/service/three/lists");
+		
 		RestTemplate rest = new RestTemplate();
 		
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Context-type", "application/json");
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
 		
-		ResponseEntity<Object> rs = rest.getForEntity("http://localhost:8080/service/three/lists", Object.class);
+		headers.add("Authorization",
+		"Bearer sk_test_LGfiAFch0kEXI3aSqYrp25ON");
 		
-		System.out.println(rs.toString());
+		ResponseEntity<Object> re = rest.exchange(uri, HttpMethod.GET, new HttpEntity<Object>(headers), Object.class);
+		
+		System.out.println(re.toString());
+		
 	}
 }
