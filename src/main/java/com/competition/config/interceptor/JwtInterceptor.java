55ebc1076b9dd31d85960f01b9435d5f80a58dbe
나규태 <kyoutae@ybtour.co.kr>
2020-04-29 13:37:28 +0900

    jwt interceptor ing

--- src/main/java/com/competition/config/interceptor/JwtInterceptor.java
+++ src/main/java/com/competition/config/interceptor/JwtInterceptor.java
@@ -1,5 +1,7 @@
 package com.competition.config.interceptor;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,6 +11,8 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import com.competition.service.token.BlackTokenService;
 import com.competition.service.token.JwtService;
 import com.competition.service.token.RefreshTokenService;
+import com.competition.service.user.UserService;
+import com.competition.user.CustomUserDetails;
 
 @SuppressWarnings("unchecked")
 public class JwtInterceptor extends HandlerInterceptorAdapter{
@@ -16,9 +20,13 @@ public class JwtInterceptor extends HandlerInterceptorAdapter{
 	@Autowired
 	private JwtService jwtService;
 	@Autowired
+	private JwtService jwtUtill;
+	@Autowired
 	private BlackTokenService blackTokenService;
 	@Autowired
 	private RefreshTokenService refreshTokenService;
+	@Autowired
+	private UserService userSerivce;
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handle) throws Exception {
@@ -28,13 +36,24 @@ public class JwtInterceptor extends HandlerInterceptorAdapter{
 		
 		// login이 되었을 경우
 		if(Access != null && Refresh != null) {
+			
+			
+			if(jwtService.validateToken(Refresh, "Refresh")) {
+				Boolean isRefresh = refreshTokenService.isRefreshToken(Refresh);
+				Boolean isBlack = blackTokenService.isBlackToken(Refresh);
 				
-			Boolean isRefresh = refreshTokenService.isRefreshToken(Refresh);
-			Boolean isBlack = blackTokenService.isBlackToken(Refresh);
+				if(!isRefresh && isBlack) return false;
+			}
 			
-			if(isRefresh && isBlack) return false;
+			if(!jwtService.validateToken(Access, "Access")) {
+				
+				String username = jwtService.getUserInfo(Access, "Access");
+				CustomUserDetails user = (CustomUserDetails) userSerivce.loadUserByUsername(username);
+				
+				response.setHeader("Access-JWT", jwtUtill.createAccessToken(request, response, user, new Date(System.currentTimeMillis() + 1 * (1000 * 60 * 30))));
+			}
+				
 			
-			if(jwtService.validateToken(Access, "Access") && jwtService.validateToken(Refresh, "Refresh")) return true;
 		}
 		
 		return true;
