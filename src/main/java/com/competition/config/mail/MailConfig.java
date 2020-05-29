5bb66813361fbcb4b2d02aa83f40dfe145e5b4ec
나규태 <kyoutae@ybtour.co.kr>
2020-05-29 16:55:30 +0900

    add

--- src/main/java/com/competition/config/mail/MailConfig.java
+++ src/main/java/com/competition/config/mail/MailConfig.java
@@ -2,20 +2,38 @@ package com.competition.config.mail;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import com.competition.jpa.model.system.config.SystemConfig;
+import com.competition.jpa.repository.system.config.SystemConfigRepository;
 
 public class MailConfig {
+	
+	@Autowired
+	private SystemConfigRepository systemConfigRepository;
+	
+	
 	@Bean
 	public JavaMailSender getJavaMailSender() {
 		JavaMailSenderImpl sender = new JavaMailSenderImpl();
-		sender.setHost("smtp.gmail.com");
-		sender.setPort(587);
 		
-		sender.setUsername("qppk123@gmail.com");
-		sender.setPassword("skrbxo12");
+		SystemConfig hostConfig = systemConfigRepository.findByConfigName("MailHost");
+		SystemConfig portConfig = systemConfigRepository.findByConfigName("MailPort");
+		
+		// smtp.gmail.com
+		sender.setHost(hostConfig.getConfigValue());
+		// 587
+		sender.setPort(Integer.parseInt(portConfig.getConfigValue()));
+		
+		
+		SystemConfig userConfig = systemConfigRepository.findByConfigName("MailUser");
+		SystemConfig pwConfig = systemConfigRepository.findByConfigName("MailPw");
+		
+		sender.setUsername(userConfig.getConfigValue());
+		sender.setPassword(pwConfig.getConfigValue());
 		
 		Properties props = sender.getJavaMailProperties();
 		props.put("mail.trasport.protocol", "smtp");
