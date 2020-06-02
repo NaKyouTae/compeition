900031fc16ab9b29898ec3b0315fd6afb051c001
나규태 <kyoutae@ybtour.co.kr>
2020-06-02 18:33:33 +0900

    add

--- src/main/java/com/competition/config/AppRunner.java
+++ src/main/java/com/competition/config/AppRunner.java
@@ -1,8 +1,7 @@
 package com.competition.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
@@ -11,13 +10,16 @@ import lombok.Data;
 
 @Data
 @Component
-public class AppRunner implements ApplicationRunner{
+public class AppRunner implements CommandLineRunner{
 	
 	@Autowired
 	Environment env;
-	
+
 	@Override
-	public void run(ApplicationArguments args) throws Exception {
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		env.getProperty("spring.data.maria.username", "root");
 	}
+	
 
 }
