3f74753bdcf7ce84105e3a852388ea08050823ab
nakyoutae <qppk@naver.com>
2020-07-14 23:02:38 +0900

    add

--- src/main/java/com/competition/jpa/model/logger/LoggerEntity.java
+++ src/main/java/com/competition/jpa/model/logger/LoggerEntity.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.Table;
@@ -20,6 +22,7 @@ public class LoggerEntity {
 	private static final long serialVersionUID = 9190376302469353485L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column
 	private long idx;
 	
